c618b858ed143360e7978cec875c406b883898e3
Cooper Hammond <kepoorh@gmail.com>
2018-12-21 12:37:42 +0000

    added a selector to the sidebar

--- src/app/components/FocusButton/FocusButtonRender.js
+++ src/app/components/FocusButton/FocusButtonRender.js
@@ -3,15 +3,18 @@ import React from 'react';
 export default function (props, state) {
 
   var classString = 'button';
+  var selectionString = 'selector';
   if ( state.isPressed ) {
     classString += ' pressed';
   }
   if ( state.isHighlighted ) {
     classString += ' highlighted';
+    selectionString += ' highlighted';
   }
 
   return (
     <div>
+      <div className={selectionString} />
       <div
         className={classString}
         onClick={this.handleClick}
