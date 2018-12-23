f11af5cefe8c9c34df88da5f12ebf401c5bdd68a
Cooper Hammond <kepoorh@gmail.com>
2018-12-23 16:11:28 +0000

    removed json data, switching to hasura

--- src/app/stores/FocusStore.js
+++ src/app/stores/FocusStore.js
@@ -4,18 +4,11 @@ import assign from 'object-assign';
 import AppDispatcher from '../dispatcher/AppDispatcher';
 import FocusConstants from '../constants/FocusConstants';
 
-//const fs = require('fs');
-//var unflatten = require('flat').unflatten;
-
+// const fs = require('fs');
+// var unflatten = require('flat').unflatten;
 
 var CHANGE_EVENT = 'change';
 
-var _data = {
-  "books": null,
-  "codes": null,
-  "students": null,
-}
-updateJSON();
 var _generalViews = [
   "Students",
   "Books",
@@ -28,7 +21,6 @@ var _focusScopes = [
 ];
 var _focusScope = _focusScopes[0];
 var _displayName = _generalViews[0];
-var _focusData = _data["students"];
 
 var FocusStore = assign({}, EventEmitter.prototype, {
 
@@ -38,10 +30,6 @@ var FocusStore = assign({}, EventEmitter.prototype, {
 
   getDisplayName: function() {
     return _displayName;
-  },
-
-  getFocusData: function() {
-    return _focusData;
   },
 
   emitChange: function() {
@@ -58,25 +46,9 @@ var FocusStore = assign({}, EventEmitter.prototype, {
 
 });
 
-function updateJSON() {
-  // var path = './src/common/data/'
-
-  // _data["books"] = unflatten(JSON.parse(fs.readFileSync(path + 'books.json', 'utf8')));
-  // _data["codes"] = unflatten(JSON.parse(fs.readFileSync(path + 'codes.json', 'utf8')));
-  // _data["students"] = unflatten(JSON.parse(fs.readFileSync(path + 'students.json', 'utf-8')));
-}
-
 function processFocusChange(focusName, focusScope) {
   _focusScope = focusScope;
   _displayName = focusName;
-
-  if ( _focusScope === "general" ) {
-    _focusData = _data[focusName]
-  } else if ( _focusScope === "student" ) {
-    _focusData = _data["students"][_displayName];
-  } else if ( _focusScope === "book" ) {
-    _focusData = _data["books"][_displayName]
-  }
 }
 
 FocusStore.dispatchToken = AppDispatcher.register(function(action) {
