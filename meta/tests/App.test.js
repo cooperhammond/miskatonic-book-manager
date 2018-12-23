bf972d7c3648c31cd60f3c1335c881e3dc3feedc
Cooper Hammond <kepoorh@gmail.com>
2018-12-23 22:13:43 +0000

    fixed test

--- meta/tests/App.test.js
+++ meta/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../app/components/AppRender/App';
+import App from '../../src/components/AppRender/App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
