78a8ae8783de58859b62df56adcd2eb6cfb8c259
Cooper Hammond <kepoorh@gmail.com>
2019-01-25 22:13:51 -0800

    Moved forms folder to PopupForm component

--- client/src/components/PopupForm/PopupFormRender.js
+++ client/src/components/PopupForm/PopupFormRender.js
@@ -3,9 +3,9 @@ import React from 'react';
 import "./popupform.scss";
 
 import StandardButton from '../StandardButton/StandardButton';
-import studentForm from '../forms/StudentForm';
-import bookForm from '../forms/BookForm';
-import codeForm from '../forms/BookForm';
+import studentForm from './forms/StudentForm';
+import bookForm from './forms/BookForm';
+import codeForm from './forms/BookForm';
 
 export default function (props, state) {
 
