9c318f0adde2d5a0c1c77c39e7035996fd55524f
Cooper Hammond <kepoorh@gmail.com>
2019-02-25 22:21:27 -0800

    Updates

---
+++ precache-manifest.8f46bf80a84f6d0e747f7e22169c3028.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e13658f608f61be15986",
+    "url": "/miskatonic-book-manager/static/css/main.43ea0b2c.chunk.css"
+  },
+  {
+    "revision": "e13658f608f61be15986",
+    "url": "/miskatonic-book-manager/static/js/main.e13658f6.chunk.js"
+  },
+  {
+    "revision": "dfce1e80af56426b9262",
+    "url": "/miskatonic-book-manager/static/js/1.dfce1e80.chunk.js"
+  },
+  {
+    "revision": "397ea666e7a9ee24da3d",
+    "url": "/miskatonic-book-manager/static/js/runtime~main.397ea666.js"
+  },
+  {
+    "revision": "d43a8c0809be47bd8fbbe9f0cf62b0c2",
+    "url": "/miskatonic-book-manager/static/media/document.d43a8c08.svg"
+  },
+  {
+    "revision": "c2e88fc1095a46c5f659baa5a5770dea",
+    "url": "/miskatonic-book-manager/static/media/books.c2e88fc1.svg"
+  },
+  {
+    "revision": "30a0d9626086eb17432d6f27bf977214",
+    "url": "/miskatonic-book-manager/static/media/student.30a0d962.svg"
+  },
+  {
+    "revision": "f01955cd4340b6066039c1d155ab06b0",
+    "url": "/miskatonic-book-manager/static/media/tag.f01955cd.svg"
+  },
+  {
+    "revision": "0145be38636574a929587fbe58a5efe7",
+    "url": "/miskatonic-book-manager/index.html"
+  }
+];
